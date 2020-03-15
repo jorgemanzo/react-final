66458644ee7d4f63a39ad1db52442c934e97200a
jorgemanzo <jmanzo203689@gmail.com>
2020-03-15 11:46:53 -0700

    Added home page content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,13 @@ import React from 'react';
 const Home = (props) => {
     return (
         <div>
-            {props.temp}
+            <h1>Weather App</h1>
+            <ul>
+                <li>Use "My Locations" to add weather locations</li>
+                <li>Visit the Settings to clear all your existing cities, and enable automatic refreshing</li>
+                <li>You can also navigate to /main/addZip/`zipCode`/`country` to automatically add a location</li>
+                <li>Visit the about page to learn how this was made</li>
+            </ul>
         </div>
     )
 }
